96513125c2fdc7d1ece6da409fb4983901b61738
Muhammad Absar <absar0920@proton.me>
2026-08-13 09:46:57 +0500

    fix: include TypeScript runtime in Windows build

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 require('ts-node').register({
     transpileOnly:true,
+    project: require('path').join(__dirname, 'tsconfig.json'),
 }); // This will register the TypeScript compiler
-require('./src'); // This will load our Typescript application 
+require('./src'); // This will load our Typescript application
